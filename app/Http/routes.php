ef1f7d07ef5f0060c9f6a06c1297767993edfb24
Roger Selwyn <rogerselwyn@hotmail.com>
2016-01-02 13:00:15 +0000

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,16 +2,22 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you will register all of the routes in an application.
+| Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', function()
-{
-    return User::all();
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::resource('species', 'SpeciesController');
+Route::resource('speciestype', 'SpeciesTypeController');
+Route::resource('location', 'LocationController');
+Route::resource('specieslocation', 'SpeciesLocationController');
+Route::resource('speciesphoto', 'SpeciesPhotoController');
+Route::resource('postcodesearch', 'PostcodeSearchController');
