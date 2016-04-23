2e18611de9af14df8cb3dc8d0977bb32d95b56ef
Roger Selwyn <rogerselwyn@hotmail.com>
2016-04-23 18:23:15 +0100

    Batched commit

--- database/migrations/2016_03_22_212200_add_fields_to_SpeciesPhoto_table.php
+++ database/migrations/2016_03_22_212200_add_fields_to_SpeciesPhoto_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToSpeciesPhoto2Table extends Migration
+class AddFieldsToSpeciesPhotoTable extends Migration
 {
     /**
      * Run the migrations.
